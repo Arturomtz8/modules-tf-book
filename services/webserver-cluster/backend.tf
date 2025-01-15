9a3286324a42363783837388d90ac203125e5347
Arturomtz8 <arturo.mtz8@gmail.com>
2025-01-14 22:44:30 -0600

    first commit

---
+++ services/webserver-cluster/backend.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  backend "s3" {
+    bucket = "state-bucket-001"
+    key    = "stage/services/webserver-cluster/terraform.tfstate"
+    region = "us-east-2"
+
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
